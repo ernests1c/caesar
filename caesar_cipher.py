0d9f355225b62b7db6daedf71e0796d733f7bf02
ernests1c <149561992+ernests1c@users.noreply.github.com>
2023-11-15 09:27:06 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,22 @@
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
+def e(t, o): # define funkciju e ''encrypted'' un tas rotaciju apli
   r = ""
-  for c in t:
-    if (a.find(c) == -1):
+  for c in t: # 
+    if (a.find(c) == -1): # ja a atrod c tad tiek nonemts viens burts
       r += c
-    else:
+    else: # ja neatrod c tad tiek pievienots viens burts
       r += (a[(a.find(c) + o) % len(a)])
   return r
 
-def d(t, o):
+def d(t, o): # define burtu d ''decrypted'' un tas rotaciju apli
   r = ""
-  for c in t:
-    if (a.find(c) == -1):
+  for c in t: # cikls kas tiek izmantots lai parveidotu burtus
+    if (a.find(c) == -1): # ja a atrod c tad tiek nonemts viens burts
       r += c
-    else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+    else: 
+      r += (a[(a.find(c) - o) % len(a)]) #
+  return r 
 
 w = """
 1. Encrypt text
@@ -25,14 +25,14 @@ w = """
 Choose mode: """
 mode = int(input(w))
 
-if mode == 1:
+if mode == 1: # ja ir ievadīts 1, tad tiek printēts teksts un prasa lai ievada rotaciju
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
   print("Encrypted: " + e(text, rotation))
-elif mode == 2:
+elif mode == 2: # ja ir ievadīts 2 tad tiek printēts teksts un prasa lai ievada rotaciju 
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
   print("Decrypted: " + d(text, rotation))
-elif mode == 3:
+elif mode == 3: # ja ir ievadīts 3 tad tiek printēts teksts un prasa lai ievada rotaciju
   print("Bruteforcing...")
   print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
